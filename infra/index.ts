ee1b294c53bccba0a08833f0c06f028c4417a0d5
Diogo <dsgmcosta@gmail.com>
2024-01-17 09:09:13 +0000

    exclude infra from nextjs vercel build

--- infra/index.ts
+++ infra/index.ts
@@ -1,4 +1,7 @@
+import * as aws from "@pulumi/aws"
 import * as awsx from "@pulumi/awsx"
 
 const repo = new awsx.ecr.Repository("awesome-repo")
 export const url = repo.url
+
+const cluster = new aws.ecs.Cluster("awesome-cluster")
--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  exclude: ["infra"],
   // images: {
   //   domains: ["picsum.photos"],
   // },
